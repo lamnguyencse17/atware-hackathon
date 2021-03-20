d22385372303225156d97d1e68ea83bd33ed5c2c
Doan Ngoc Thinh <thinhdoanngoc@gmail.com>
2021-03-20 22:06:47 +0700

    Search bar updated (stable version)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Button from "@material-ui/core/Button";
+import SearchIcon from "@material-ui/icons/Search";
 
 export default function App() {
+	// District
 	const districts = [
+		"Binh Chanh District",
+		"Binh Tan District",
+		"Binh Thanh District",
+		"Can Gio District",
+		"Cu Chi District",
 		"District 1",
-		"District 2",
 		"District 3",
 		"District 4",
 		"District 5",
@@ -11,29 +20,65 @@ export default function App() {
 		"District 7",
 		"District 8",
 		"District 9",
-		"District 10",
 		"District 11",
+		"District 12",
+		"Go Vap District",
+		"Hoc Mon District",
+		"Nha Be District",
+		"Phu Nhuan District",
+		"Tan Binh District",
+		"Tan Phu District",
+		"Thu Duc District",
 	];
-	const hours = [...Array(24).keys()];
+	const date = new Date();
+	// Time
+	console.log(date.getHours());
+	const times = [...Array(24).keys()]
+		.filter((ele) => ele = date.getHours())
+		.map((ele) => (ele / 10 >= 1 ? "" : "0") + ele.toString() + ":00");
 	return (
 		<div className='container mx-auto'>
-			<div className='mt-10 '>
-				<div className='px-2 pt-5 pb-2 bg-gray-100'>
-					<h1 className='text-2xl'>Find a place to go</h1>
+			<div className='mt-10'>
+				<div className='bg-gray-100 p-2 border-l-8'>
 					<form className='grid w-full grid-cols-5'>
-						<select className='col-span-2'>
-							{districts.map((district) => (
-								<option value={district} key={district}>
-									{district}
-								</option>
-							))}
-						</select>
-						<label className='flex items-center justify-center col-span-2'>
-							Time
-						</label>
-						<button className='p-2 bg-red-800 rounded-md' type='submit'>
-							Search now
-						</button>
+						<div className='col-span-2 p-2'>
+							<Autocomplete
+								color='primary'
+								id='address'
+								options={districts}
+								getOptionLabel={(option) => option}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										label='Place to go'
+										variant='outlined'
+									/>
+								)}
+							/>
+						</div>
+						<div className='col-span-2 p-2'>
+							<Autocomplete
+								color='primary'
+								id='time'
+								options={times}
+								getOptionLabel={(option) => option}
+								renderInput={(params) => (
+									<TextField {...params} label='Time' variant='outlined' />
+								)}
+							/>
+						</div>
+						<div className='col-span-1 p-2 flex justify-center items-center'>
+							<Button color="primary">
+								<div className='text-xl primary min-w-100 min-h-100'>
+									<SearchIcon
+										color='text-xl'
+										disableFocusRipple={true}
+										disableRipple={true}
+									/>
+									{"Search now"}
+								</div>
+							</Button>
+						</div>
 					</form>
 				</div>
 			</div>
