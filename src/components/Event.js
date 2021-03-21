7716f26fca1063ff70962cbd553fd5e740718514
Doan Ngoc Thinh <thinhdoanngoc@gmail.com>
2021-03-21 11:47:06 +0700

    Detail of the event

--- src/components/Event.js
+++ src/components/Event.js
@@ -16,8 +16,39 @@ export default function Event() {
 	// 	return <></>;
 	// }
 	return (
-		<div>
-			{/* <h1>Event: {event.title}</h1>
+		<div className='container m-auto bg-gray-100'>
+			<div className='text-md'>Appointment's detail</div>
+			<div className='mt-4 flex flex-row'>
+				<table class='table-auto'>
+					<thead>
+						<th>
+							<td>Event</td>
+							<td>Description</td>
+							<td>Date</td>
+							<td>Time</td>
+							<td>Address</td>
+							<td>District</td>
+							<td>Host's name</td>
+							<td>Phone number</td>
+						</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			</div>
+			{/* 
+			<h1>Event: {event.title}</h1>
 			<h1>Date: {event.date}</h1>
 			<h1>Time: {event.time}:00</h1>
 			<h1>Address: {event.address}</h1>
